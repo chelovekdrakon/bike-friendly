37336f821cc1dfdf5e5dea04a3e215d2037d7a03
Your Name <you@example.com>
2018-10-07 18:57:59 +0300

    add

--- imports/ui/pages/map/Map.js
+++ imports/ui/pages/map/Map.js
@@ -1,90 +1,133 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 
-import { GoogleMap } from './google-map';
+import { GoogleMap } from "./google-map";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Button = styled.button`
-    border: 0.2rem solid black;
-    border-radius: 0.3rem;
-    margin: 1rem 0;
-    padding: 0.4rem 2rem;
-    font-size: 1.5rem;
-    min-width: 10%;
+  border: 0.2rem solid black;
+  border-radius: 0.3rem;
+  margin: 1rem 0;
+  padding: 0.4rem 2rem;
+  font-size: 1.5rem;
+  min-width: 10%;
 
-    :focus {
-        outline: none;
-    }
+  :focus {
+    outline: none;
+  }
 
-    :hover {
-        cursor: pointer;
-        color: red;
-        border-color: red;
-    }
+  :hover {
+    cursor: pointer;
+    color: red;
+    border-color: red;
+  }
 `;
 
 class Map extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            place: null
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      place: null
+    };
 
-        this.onPlacePick = this.onPlacePick.bind(this);
-        this.handleVote = this.handleVote.bind(this)
-    }
+    this.onPlacePick = this.onPlacePick.bind(this);
+    this.handleVote = this.handleVote.bind(this);
+  }
 
-    onPlacePick({ placeId, latLng }) {
-        if (placeId) {
-            console.log('place id: ', placeId);
+  onPlacePick({ placeId, latLng }) {
+    if (placeId) {
+      console.log("place id: ", placeId);
 
-            const lat = latLng.lat();
-            const lng = latLng.lng();
+      const lat = latLng.lat();
+      const lng = latLng.lng();
 
-            console.log('lat: ', lat);
-            console.log('lng: ', lng);
+      console.log("lat: ", lat);
+      console.log("lng: ", lng);
 
-            this.setState({
-                place: {
-                    lat,
-                    lng,
-                    placeId
-                }
-            });
-
-            // fetch(`https://maps.googleapis.com/maps/api/place/details/output?key=AIzaSyAVFInxP4hbkJFbLz_L9XzRYUb3RaggzQc&placeid=${placeId}`, { mode: 'no-cors' }));
+      this.setState({
+        place: {
+          lat,
+          lng,
+          placeId
         }
+      });
+
+      // fetch(`https://maps.googleapis.com/maps/api/place/details/output?key=AIzaSyAVFInxP4hbkJFbLz_L9XzRYUb3RaggzQc&placeid=${placeId}`, { mode: 'no-cors' }));
+    }
+  }
+
+  handleVote() {
+    function sendRequest(status, data) {
+      const constPermlik = "test12345";
+      const acc = "test3",
+        pass = "qwerty12345",
+        wif = "5Hvp79CaQrYUD9d33VvdtWY5BhyimS4t5vMDCBJE1WsTUUPuu1F",
+        author = "test3",
+        parentPermlink = "let-me-live-let-me-breath",
+        permLink = "veloboys",
+        body = data,
+        title = "TITLE";
+      if (status == "comment") {
+        this.parentAuthor = author;
+        this.parentPermlink = parentPermlink;
+        this.permlink = String(Math.floor(Math.random() * (10000 - 1 + 1)) + 1);
+      } else {
+        this.parentAuthor = "";
+        this.parentPermlink = parentPermlink;
+        this.permlink = permLink;
+      }
+
+      window.golos.broadcast.comment(
+        wif,
+        this.parentAuthor,
+        this.parentPermlink,
+        author,
+        this.permlink,
+        title,
+        body,
+        "",
+        function(err, result) {
+          if (!err) {
+            console.log(result);
+          } else console.error(err);
+        }
+      );
     }
 
-    handleVote() {
-        if (this.state.place) {
-            console.log(this.state.place);
-        } else {
-            console.log('choose the place');
-        }
+    if (this.state.place) {
+      console.log(this.state.place);
+      window.golos.api.getContent(acc, constPermlik, function(err, result) {
+        console.log(result);
+        result.id == 0
+          ? sendRequest("post", this.state.place)
+          : sendRequest("comment", this.state.place);
+        if (err) console.error(err);
+      });
+    } else {
+      console.log("choose the place");
     }
-    
-    render() {
-        return (
-            <Container>
-                <GoogleMap 
-                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAVFInxP4hbkJFbLz_L9XzRYUb3RaggzQc&v=3.exp&libraries=geometry,drawing,places"
-                    loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement={<div style={{ height: `400px` }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
-                    onPlacePick={this.onPlacePick}
-                    {...this.props}
-                />
-                <Button onClick={this.handleVote}>VOTE</Button>
-            </Container>
-        );
-    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <GoogleMap
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAVFInxP4hbkJFbLz_L9XzRYUb3RaggzQc&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          onPlacePick={this.onPlacePick}
+          {...this.props}
+        />
+        <Button onClick={this.handleVote}>VOTE</Button>
+      </Container>
+    );
+  }
 }
 
 export default Map;
-  
