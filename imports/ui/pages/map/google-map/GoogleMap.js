94111b38a73ec861e0ed869b249f3d5733836e8d
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2018-10-09 04:47:39 +0300

    fix markers on google map

--- imports/ui/pages/map/google-map/GoogleMap.js
+++ imports/ui/pages/map/google-map/GoogleMap.js
@@ -1,24 +1,94 @@
 import React from 'react';
-import { compose, withProps } from 'recompose';
-import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
-
-import { Meteor } from 'meteor/meteor';
+import { SearchBox } from 'react-google-maps/lib/components/places/SearchBox';
 
 import styled from 'styled-components';
 
-const Div = styled.div`
-    background-color: white;
+const SearchInput = styled.input`
+    border: 1px solid transparent;
+    width: 240px;
+    height: 32px;
+    margin-top: 1rem;
+    padding: 1.9rem;
+    border-radius: 3px;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+    font-size: 14px;
+    outline: none;
+    text-overflow: ellipsis;
 `;
 
-const MapPage = withScriptjs(
-    withGoogleMap(props => (
-        <GoogleMap defaultZoom={13} defaultCenter={{ lat: 40.415033, lng: -3.723795 }} onClick={props.onPlacePick}>
-            <Marker position={{ lat: 40.415033, lng: -3.723795 }} />
-        </GoogleMap>
-    ))
-);
+class _GoogleMap extends React.PureComponent {
+    state = {
+        markers: [],
+        bound: null,
+        center: {
+            lat: 40.415033,
+            lng: -3.723795,
+        },
+    };
+
+    refMap = node => {
+        this.map = node;
+    };
+
+    refSearchBox = node => {
+        this.searchBox = node;
+    };
+
+    onPlacesChanged = () => {
+        const places = this.searchBox.getPlaces();
+        const bounds = new google.maps.LatLngBounds();
+
+        places.forEach(place => {
+            if (place.geometry.viewport) {
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+
+        const nextMarkers = places.map(place => ({
+            position: place.geometry.location,
+        }));
+
+        const nextCenter = _.get(nextMarkers, '0.position', this.map.getCenter());
+
+        this.setState({
+            center: nextCenter,
+            markers: nextMarkers,
+        });
+    };
+
+    render() {
+        return (
+            <div style={this.props.containerStyles}>
+                <GoogleMap ref={this.refMap} defaultZoom={13} defaultCenter={this.state.center}>
+                    <SearchBox
+                        ref={this.refSearchBox}
+                        bounds={this.map ? this.map.getBounds() : null}
+                        controlPosition={google.maps.ControlPosition.TOP_LEFT}
+                        onPlacesChanged={this.onPlacesChanged}
+                    >
+                        <SearchInput type="text" placeholder="Search for places" />
+                    </SearchBox>
+                    {this.state.markers.map((marker, index) => (
+                        <Marker
+                            key={index}
+                            position={{
+                                lat: marker.position.lat(),
+                                lng: marker.position.lng(),
+                            }}
+                        />
+                    ))}
+                </GoogleMap>
+            </div>
+        );
+    }
+}
+
+const MapPage = withScriptjs(withGoogleMap(_GoogleMap));
 
 MapPage.defaultProps = {};
 
